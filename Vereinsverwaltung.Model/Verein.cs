f9559bcdba642fc519d89e771e2dc676f4ad1c2a
JD <65065236+D0mnik@users.noreply.github.com>
2020-10-04 16:13:27 +0200

    implements Model / WPF / Utils / csv

---
+++ Vereinsverwaltung.Model/Verein.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Vereinsverwaltung.Model
+{
+    public class Verein
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Fullname
+        {
+            get
+            {
+                return Firstname + " " + Lastname;
+            }
+        }
+        public DateTime Birthday { get; set; }
+
+    }
+}
